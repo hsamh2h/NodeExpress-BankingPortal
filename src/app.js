7a1536d93a125e3dd7c2a680290acfd39fdea49d
Husam Hamdan <husamhamdan@Husams-MacBook-Pro.local>
2023-01-20 17:35:53 -0500

    First commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const {accounts, users, writeJSON} = require('./data.js');
+const accountRoutes = require('./routes/accounts.js');
+const servicesRoutes = require('./routes/services.js');
+
+
+
+const app = express();
+
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Account Summary', accounts : accounts});
+});
+
+app.listen(3000, () => {
+    console.log("PS Project Running on port 3000!");
+  });
